bf211f51fe05ce8890c76291b172a886035ed3d7
Allan Hardy <al_hardy@live.com.au>
2018-02-27 13:50:03 +1100

    updating ensure cosmos db task

--- src/Vsts.Release.EnsureCosmosDbDatabase/Ensure-CosmosDbDatabase.ps1
+++ src/Vsts.Release.EnsureCosmosDbDatabase/Ensure-CosmosDbDatabase.ps1
@@ -16,30 +16,30 @@ try {
 	
 	Install-Module -Name CosmosDB -Force -Verbose -Scope CurrentUser
 
-	Write-Host "##[command](New-CosmosDbConnection -Account $CosmosDBAccount -Database $DatabaseName -ResourceGroup $ResourceGroupName -MasterKeyType 'SecondaryMasterKey')"
-	$cosmosDbConnection = New-CosmosDbConnection -Account $CosmosDBAccount -Database $DatabaseName -ResourceGroup $ResourceGroupName -MasterKeyType 'SecondaryMasterKey'
+	Write-Host "##[command](New-CosmosDbContext -Account $CosmosDBAccount -Database $DatabaseName -ResourceGroup $ResourceGroupName -MasterKeyType 'SecondaryMasterKey')"
+	$cosmosDbContext = New-CosmosDbContext -Account $CosmosDBAccount -Database $DatabaseName -ResourceGroup $ResourceGroupName -MasterKeyType 'SecondaryMasterKey'
 	
 	try {
-		Write-Host "##[command](Get-CosmosDbDatabase -Connection $cosmosDbConnection -Id $DatabaseName)"
-		Get-CosmosDbDatabase -Connection $cosmosDbConnection -Id $DatabaseName
+		Write-Host "##[command](Get-CosmosDbDatabase -Context $cosmosDbContext -Id $DatabaseName)"
+		Get-CosmosDbDatabase -Context $cosmosDbContext -Id $DatabaseName
 	} catch {
 		if (-not ($_.Exception.Response.StatusCode.value__ -eq "404")) {
 			throw $_.Exception
 		} else {
-			Write-Host "##[command](New-CosmosDbDatabase -Connection $cosmosDbConnection -Id $DatabaseName)"
-			New-CosmosDbDatabase -Connection $cosmosDbConnection -Id $DatabaseName
+			Write-Host "##[command](New-CosmosDbDatabase -Context $cosmosDbContext -Id $DatabaseName)"
+			New-CosmosDbDatabase -Context $cosmosDbContext -Id $DatabaseName
 		}
 	}
 
 	try {
-		Write-Host "##[command](Get-CosmosDbCollection -Connection $cosmosDbConnection -Id $CollectionName)"
-		Get-CosmosDbCollection -Connection $cosmosDbConnection -Id $CollectionName
+		Write-Host "##[command](Get-CosmosDbCollection -Context $cosmosDbContext -Id $CollectionName)"
+		Get-CosmosDbCollection -Context $cosmosDbContext -Id $CollectionName
 	} catch {
 		if (-not ($_.Exception.Response.StatusCode.value__ -eq "404")) {
 			throw $_.Exception
 		} else {
-			Write-Host "##[command](New-CosmosDbCollection -Connection $cosmosDbConnection -Id $CollectionName -PartitionKey $CollectionPartitionKey -OfferThroughput $CollectionRUs)"
-			New-CosmosDbCollection -Connection $cosmosDbConnection -Id $CollectionName -PartitionKey $CollectionPartitionKey -OfferThroughput $CollectionRUs
+			Write-Host "##[command](New-CosmosDbCollection -Context $cosmosDbContext -Id $CollectionName -PartitionKey $CollectionPartitionKey -OfferThroughput $CollectionRUs)"
+			New-CosmosDbCollection -Context $cosmosDbContext -Id $CollectionName -PartitionKey $CollectionPartitionKey -OfferThroughput $CollectionRUs
 		}
 	}
 } finally {
